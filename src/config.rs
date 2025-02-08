9f87580f9715722c64eeca548f7124d3c0e129ce
cq <575893469@qq.com>
2025-02-08 17:13:02 +0800

    reset password

--- src/config.rs
+++ src/config.rs
@@ -68,7 +68,14 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_DISPLAY_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref DEFAULT_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
-    pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
+    pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
+        let mut map = HashMap::new();
+        // 使用编译时宏获取环境变量
+        if let Some(password) = option_env!("RUSTDESK_DEFAULT_PASSWORD") {
+            map.insert("password".to_string(), password.to_string());
+        }
+        RwLock::new(map)
+    };
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
 }
 
